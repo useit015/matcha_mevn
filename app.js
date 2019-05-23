195b8139526280cb68e01c2a00d3e9b0ce8c38df
Oussama Nahiz <onahiz@e2r9p14.1337.ma>
2019-05-23 03:07:27 +0000

    commited

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
+const express = require('express')
 const mysql = require('mysql')
 const cors = require('cors')
 const path = require('path')
 const port = process.env.PORT || 8080
+
+const db = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '0O*ussama',
+	database: 'slim'
+})
+
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql Connected...');
+});
+
+const app = express()
 
 app.use(cors())
 
@@ -14,18 +29,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')))
 
 app.get('/api/users', (req, res) => {
-	const conn = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: '0O*ussama',
-		database: 'slim'
-	})
-	conn.connect()
-	conn.query('SELECT * FROM users, images WHERE users.id = images.user_id AND images.profile = 1', (err, rows, fields) => {
+	db.query('SELECT * FROM users, images WHERE users.id = images.user_id AND images.profile = 1', (err, rows, fields) => {
 		if (err) throw err
 		res.json(rows)
 	})
-	conn.end()
 })
 
 app.listen(port, () => console.log('the server has started'))
