9d1f53004c3fb8a1e02ffa45059c8d597e3f7930
Oussama Nahiz <onahiz@e2r9p14.1337.ma>
2019-05-24 01:58:42 +0000

    commited

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mysql = require('mysql')
 const router = express.Router()
+const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 const db = mysql.createConnection({
@@ -16,11 +17,32 @@ db.connect(err => {
 })
 
 router.post('/login', (req, res) => {
-	db.query(`SELECT * FROM users WHERE users.username = '${req.body.username}' AND verified = 1`, (err, rows, fields) => {
+	const sql = `SELECT * FROM users WHERE users.username = '${req.body.username}' AND verified = 1`
+	db.query(sql, (err, rows) => {
 		if (err) throw err
 		jwt.sign({ user: rows[0] }, 'secretkey', { expiresIn: '30s' }, (err, token) => {
+			if (err) throw err
 			res.json({ token })
 		})
+	})
+})
+
+router.post('/add', (req, res) => {
+	const salt = bcrypt.genSaltSync(10)
+	const user = {
+		first_name: req.body.first_name,
+		last_name: req.body.last_name,
+		username: req.body.username,
+		password: bcrypt.hashSync(req.body.password, salt),
+		email: req.body.email,
+		vkey: bcrypt.genSaltSync(10)
+	}
+	// MUST VALIDATE USER !!!!
+	const sql = `INSERT INTO users (first_name, last_name, username, email, password, vkey)
+		VALUES ('${user.first_name}', '${user.last_name}', '${user.username}', '${user.email}', '${user.password}', '${user.vkey}')`
+	db.query(sql, err => {
+		if (err) throw err
+		res.json('User Added')
 	})
 })
 
