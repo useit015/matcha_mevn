f6ca3d0a64e35bc4ffe7b9171d88cc56f43c4983
Oussama Nahiz <onahiz@e2r9p14.1337.ma>
2019-05-24 01:22:27 +0000

    commited

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ const app = express()
 
 app.use(cors())
 
-app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use('/api/users', users)
 app.use('*/css',express.static(__dirname + '/public/css'))
 app.use('*/js',express.static(__dirname + '/public/js'))
+
 app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')))
 
 app.listen(port, () => console.log(`The server has started on port -> ${port}`))
