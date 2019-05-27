42e51651a3c11a083ed2c09fb6492811713278f1
Oussama Nahiz <onahiz@e2r9p14.1337.ma>
2019-05-27 04:43:07 +0000

    commited

--- app.js
+++ app.js
@@ -5,51 +5,15 @@ const path = require('path')
 const users = require('./routes/api/users')
 const port = process.env.PORT || 8080
 const app = express()
-const mailer = require('express-mailer')
 
 app.use(cors())
 
-app.set('views', __dirname + '/views')
-app.set('view engine', 'pug')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-
-mailer.extend(app, {
-	from: 'ousstest015@gmail.com',
-	host: 'smtp.gmail.com', // hostname
-	secureConnection: true, // use SSL
-	port: 465, // port for secure SMTP
-	transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
-	auth: {
-		user: 'ousstest015@gmail.com', // generated ethereal user
-		pass: 'fuck3dupsh17' // generated ethereal password
-	}
-})
-
 app.use('/api/users', users)
 app.use('/static', express.static(`${__dirname}/public`))
-
-app.get('/mail', (req, res) => {
-	var mailOptions = {
-		to: 'useit015@gmail.com',
-		subject: 'Email from SMTP sever',
-		user: {
-			name: 'Arjun PHP',
-			message: 'Welcome to arjunphp.com'
-		}
-	  }
-	 
-	// Send email.
-	app.mailer.send('email', mailOptions, function (err, message) {
-		if (err) {
-			console.log(err)
-			return res.send('There was an error sending the email')
-		}
-		return res.send('Email has been sent!')
-	})
-})
 
 app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')))
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -179,30 +179,34 @@ router.post('/login', (req, res) => {
 	})
 })
 
-// async function sendMail () {
-// 	// create reusable transporter object using the default SMTP transport
-// 	let transporter = nodemailer.createTransport({
-// 		host: 'smtp.gmail.com',
-// 		port: 587,
-// 		secure: false, // true for 465, false for other ports
-// 		auth: {
-// 			user: 'ousstest015@gmail.com', // generated ethereal user
-// 			pass: 'fuck3dupsh17' // generated ethereal password
-// 		}
-// 	})
+async function sendMail () {
+	// create reusable transporter object using the default SMTP transport
+	let transporter = nodemailer.createTransport({
+		host: 'smtp.gmail.com',
+		port: 587,
+		secure: false, // true for 465, false for other ports
+		auth: {
+			user: 'ousstest015@gmail.com', // generated ethereal user
+			pass: 'fuck3dupsh17' // generated ethereal password
+		}
+	})
 
-// 	// send mail with defined transport object
-// 	let info = await transporter.sendMail({
-// 		from: '"Fred Foo 👻" <foo@example.com>', // sender address
-// 		to: "useit015@gmail.com", // list of receivers
-// 		subject: "Hello ✔", // Subject line
-// 		text: "Hello world?", // plain text body
-// 		html: "<b>Hello world?</b>" // html body
-// 	})
+	// send mail with defined transport object
+	let info = await transporter.sendMail({
+		from: '"Fred Foo 👻" <foo@example.com>', // sender address
+		to: "useit015@gmail.com", // list of receivers
+		subject: "Hello ✔", // Subject line
+		text: "Hello world?", // plain text body
+		html: "<b>Hello world?</b>" // html body
+	})
 
-// 	console.log("Message sent: %s", info.messageId)
-// 	console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
-// }
+	console.log("Message sent: %s", info.messageId)
+	console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
+}
+
+router.get('/mail', (req, res) => {
+	sendMail()
+})
 
 router.post('/add', (req, res) => {
 	// ! MUST VALIDATE INPUT !!!!
