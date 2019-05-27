8daadcbe4f8152e05564cc7b9d621353c2fb2f46
Oussama Nahiz <onahiz@e2r9p14.1337.ma>
2019-05-27 03:54:25 +0000

    commited

--- routes/api/users.js
+++ routes/api/users.js
@@ -194,7 +194,7 @@ async function sendMail () {
 	// send mail with defined transport object
 	let info = await transporter.sendMail({
 		from: '"Fred Foo 👻" <foo@example.com>', // sender address
-		to: "bar@example.com, baz@example.com", // list of receivers
+		to: "useit015@gmail.com", // list of receivers
 		subject: "Hello ✔", // Subject line
 		text: "Hello world?", // plain text body
 		html: "<b>Hello world?</b>" // html body
