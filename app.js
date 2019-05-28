c4899df1a25a1d29b88b1395ff6bfd5a728afdc9
Oussama Nahiz <onahiz@e2r9p14.1337.ma>
2019-05-28 04:39:56 +0000

    commited

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const server = http.createServer(app)
 
 const io = socketIo(server)
 
-const users = []
+let users = []
 
 io.on('connection', socket => {
 	console.log('New client connected', socket.id)
