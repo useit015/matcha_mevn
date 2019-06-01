c2ec5e5fd03a162342525259b5dd36f9c1a6f913
Oussama Nahiz <onahiz@e2r9p14.1337.ma>
2019-06-01 03:40:52 +0000

    commited

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ io.use((socket, next) => {
 })
 
 io.on('connection', socket => {
-	console.log('I am the socket --> ', socket)
+	console.log('I am the socket --> ', socket.connected)
 	console.log('New client connected', socket.id)
 	console.log('> users are', users)
 	online.push(socket.id)
