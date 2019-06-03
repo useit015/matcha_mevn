ad79cf7584e8a75a542e6fe517e8a9eba5e66625
Yahya EZ-ZAINABI <yezzainabi@gmail.com>
2019-06-03 20:46:47 +0000

    Fix login : change result variable to rv

--- routes/api/users.js
+++ routes/api/users.js
@@ -140,8 +140,8 @@ router.post('/login', async (req, res) => {
 		const result = await pool.query(sql, [req.body.username])
 		if (result.length && result[0].verified) {
 			const user = result[0]
-			const r = bcrypt.compare(req.body.password, user.password)
-			if (r) {
+			const rv = bcrypt.compare(req.body.password, user.password)
+			if (rv) {
 				user.token = crypto.randomBytes(10).toString('hex')
 				user.tokenExpiration = moment().add(2, 'hours').format('YYYY-MM-DD HH:mm:ss')
 				let sql = `UPDATE users SET token = ?, tokenExpiration = ? WHERE id = ?`
