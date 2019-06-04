f121bfd06751d70d88e7405463266a5411b900b0
useit015 <useit015@gmail.com>
2019-06-04 05:48:34 +0000

    fuck up test

--- routes/api/chat.js
+++ routes/api/chat.js
@@ -3,7 +3,29 @@ const pool = require('../../utility/database')
 
 router.post('/all', async (req, res) => {
 	try {
-		const sql = `SELECT * FROM conversations WHERE id_user1 = ? OR id_user2 = ?`
+		const sql = `SELECT
+						id_conversation, last_update,
+						users.username as username,
+						users.first_name as first_name,
+						users.last_name as last_name,
+						users.status as status,
+						images.name as profile_image
+					FROM conversations
+					INNER JOIN users ON conversations.id_user2 = users.id
+					INNER JOIN images ON conversations.id_user2 = images.user_id
+					WHERE conversations.id_user1 = ?
+					UNION SELECT
+						id_conversation, last_update,
+						users.username as username,
+						users.first_name as first_name,
+						users.last_name as last_name,
+						users.status as status,
+						images.name as profile_image
+					FROM conversations
+					INNER JOIN users ON conversations.id_user1 = users.id
+					INNER JOIN images ON conversations.id_user1 = images.user_id
+					WHERE conversations.id_user2 = ?
+					ORDER BY last_update DESC`
 		const result = await pool.query(sql, [req.body.id, req.body.id])
 		res.json(result)
 	} catch (err) {
@@ -29,7 +51,7 @@ router.post('/send', async (req, res) => {
 			id_from: req.body.id_from,
 			message: req.body.message
 		}
-		const sql = `INSERT INTO chat (id_conversation, id_from, mesaage) VALUES (?, ?, ?)`
+		const sql = `INSERT INTO chat (id_conversation, id_from, message) VALUES (?, ?, ?)`
 		await pool.query(sql, Object.values(msg))
 		res.json('Message added')
 	} catch (err) {
