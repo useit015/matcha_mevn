51ed84510f095fd70a9f32b8ceb07990c4c88af8
useit015 <useit015@gmail.com>
2019-06-12 08:05:16 +0000

    cleaning

--- client/config/index.js
+++ client/config/index.js
@@ -38,11 +38,11 @@ module.exports = {
 
 	build: {
 		// Template for index.html
-		index: path.resolve(__dirname, '../dist/index.html'),
+		index: path.resolve(__dirname, '../../public/index.html'),
 
 		// Paths
-		assetsRoot: path.resolve(__dirname, '../dist'),
-		assetsSubDirectory: 'static',
+		assetsRoot: path.resolve(__dirname, '../../public'),
+		assetsSubDirectory: '.',
 		assetsPublicPath: '/',
 
 		/**
