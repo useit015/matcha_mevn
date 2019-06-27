c32f3802b43e9b7863188be7615e1345639211ef
useit015 <useit015@gmail.com>
2019-06-27 22:47:38 +0000

    commited

--- utility/validate.js
+++ utility/validate.js
@@ -2,14 +2,14 @@ const validateInput = (input, type) => {
 	if (!input) return false
 	switch (type) {
 		case 'username':
-			return !(input.length < 8 || input.length > 25)
+			return !(input.length < 8 || input.length > 25 || /[^a-zA-Z0-9]+/.test(input))
 		case 'password':
 			return !(input.length < 8 || input.length > 255 || !(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(input)))
 		case 'email':
 			return (/.+@.+/.test(input))
 		case 'fname':
 		case 'lname':
-			return (input.length >= 3 && input.length <= 255)
+			return (input.length >= 3 && input.length <= 255 && !(/[^a-zA-Z]+/.test(input)))
 		case 'looking':
 			return !(input != 'male' && input != 'female' && input != 'both')
 		case 'gender':
